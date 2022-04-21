29cd59741477c9cb9ba78b5f7bda4427db2a12e1
Feridun Canselen <canselenferidun@gmail.com>
2022-04-21 16:11:07 +0300

    added a temporary readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello Wrkd</div>
+      <div>Hello</div>
     </div>
   );
 }
